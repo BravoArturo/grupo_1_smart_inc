909c78f3747f6a0b0c9c2a85f6ba8b3f5cb5b61e
DarioGomez44 <dario_eg@outlook.com>
2022-10-09 13:44:35 -0300

    diseño de login inicial

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const publicPath = path.join(__dirname, '/public')
 app.use(express.static(publicPath))
 
 app.listen(3000, () => {
-  console.log('Servidor OK en puerto 3000')
+  console.log(`Servidor corrien en el puerto http://localhost:3000/`)
 })
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'))
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/login.html'))
 })
