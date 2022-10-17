918794abd381d5bb82cbfae73509c5b2f5c6d2c6
DFCH9503 <dafechaparro@hotmail.com>
2022-10-17 16:27:10 -0500

    index.html responsive-revisar header y footer

--- app.js
+++ app.js
@@ -13,3 +13,11 @@ app.listen(3000, () => {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'))
 })
+
+app.get('/login',(req,res)=>{
+  res.sendFile(path.join(__dirname,'/views/login.html'))
+});
+
+app.get('/productCart',(req,res)=>{
+  res.sendFile(path.join(__dirname,'/views/productCart.html'))
+});
