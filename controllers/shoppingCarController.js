7e1c004d745024d519c8dd77e0bf36feef2cd143
BravoArturo <aarturoo97@gmail.com>
2022-10-19 14:08:49 -0300

    add controller and routes folder

---
+++ controllers/shoppingCarController.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+const publicPath = path.join(__dirname, "/public");
+const controller = {
+  index: (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/shoppingCar.html"));
+  },
+};
+
+module.exports = controller;
