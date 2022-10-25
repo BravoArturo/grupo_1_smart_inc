1f61e9e8c01e09d0b6e2164a653c86d06da22a3d
Brian Petralito <brianpetralito@gmail.com>
2022-10-25 20:25:25 -0300

    subiendo al repo

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const express = require('express')
-const path = require('path')
+const express = require ("express");
+const path = require ("path");
 
-const app = express()
-const publicPath = path.join(__dirname, '/public')
+const app = express ()
+const publicPath = path.join(__dirname, "./public")
+app.use (express.static(publicPath))
 
-app.use(express.static(publicPath))
-
-app.listen(3000, () => {
-  console.log('Servidor OK en puerto 3000')
+app.listen (3000, () => {
+    console.log("Servidor corriendo")
 })
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'))
+app.get ("/", (req, res) => {
+    res.sendFile (path.join(__dirname,"./views/product-detail.html"))
 })
