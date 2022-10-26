0aea4be351c0edaee018f6ac1482f3f413bef962
BravoArturo <aarturoo97@gmail.com>
2022-10-26 15:04:24 -0300

    make product a component and pass data throw the include

--- controllers/shoppingCarController.js
+++ controllers/shoppingCarController.js
@@ -1,16 +1,34 @@
 const path = require('path')
 
-const publicPath = path.join(__dirname, '/public')
-
 // const controller = {
 //   index: (req, res) => {
 //     res.sendFile(path.join(__dirname, '../views/shoppingCar.html'))
 //   },
 // }
 
+const productos = [
+  {
+    imgSource: '/images/descarga(1).jfif',
+    description: 'descripción',
+    price: 5000,
+  },
+  {
+    imgSource: '/images/descarga.jfif',
+    description: 'descripción',
+    price: 5000,
+  },
+  {
+    imgSource: '/images/images.jfif',
+    description: 'descripción',
+    price: 5000,
+  },
+]
 const controller = {
   index: (req, res) => {
-    res.render('shoppingCar')
+    res.render('shoppingCar', { productos: productos })
+  },
+  bienvenido: (req, res) => {
+    res.render('index')
   },
 }
 
--- routes/shoppingCar.js
+++ routes/shoppingCar.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const controller = require('../controllers/shoppingCarController')
 
 router.get('/', controller.index)
+router.get('/bienvenido', controller.bienvenido)
 
 module.exports = router
