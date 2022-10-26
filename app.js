511c9d547e9b359a1eb1b9d07c7fd64669511470
DarioGomez44 <dario_eg@outlook.com>
2022-10-26 15:04:59 -0300

    modificando footers y headers

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const publicPath = path.join(__dirname, '/public')
 const routerLogin = require('./routers/loginRoute')
 
 app.use(express.static(publicPath))
-
+app.set('view engine','ejs')
 app.listen(3000, () => {
   console.log(`Servidor corrien en el puerto http://localhost:3000/`)
 })
@@ -15,7 +15,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'))
 })
 
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/login.html'))
-})
+// app.get('/login', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/views/login.html'))
+// })
 app.use('/login-mobil', routerLogin)
+
--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,7 +4,7 @@ const publicPath = path.join(__dirname, '/public')
 
 const controller = {
   index: (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/login.html'))
+    res.render('login')
   },
 }
 
