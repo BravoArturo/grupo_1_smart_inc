1a6c13f49a6c3f1d2a98e4370bd29d65af440c4b
BravoArturo <aarturoo97@gmail.com>
2022-10-29 13:19:00 -0300

    se cambio la carpeta routers por routes

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const path = require('path')
 
 const app = express()
 const publicPath = path.join(__dirname, '/public')
-const routerLogin = require('./routers/loginRoute')
+const routerLogin = require('./routes/loginRoute')
 
 app.use(express.static(publicPath))
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 app.listen(3000, () => {
   console.log(`Servidor corrien en el puerto http://localhost:3000/`)
 })
@@ -19,4 +19,3 @@ app.get('/', (req, res) => {
 //   res.sendFile(path.join(__dirname, '/views/login.html'))
 // })
 app.use('/login-mobil', routerLogin)
-
