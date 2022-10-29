4b6682ae6168abe5de197c29eef7e1efc1df1478
BravoArturo <aarturoo97@gmail.com>
2022-10-29 11:04:09 -0300

    structure created for editAddProduct

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const publicPath = path.join(__dirname, '/public')
-
 const routeIndex = require('./routes/index')
 const routeShoppingCar = require('./routes/shoppingCar')
-
+const routeProduct = require('./routes/product')
 app.use(express.static(publicPath))
 app.set('view engine', 'ejs')
 
 app.listen(3000, () => {
-  console.log('Servidor OK en puerto','http://localhost:3000')
+  console.log('Servidor OK en puerto', 'http://localhost:3000')
 })
 
 // DESCOMENTAR LÍNEAS 16-19 SI NO FUNCIONA EL .use DE LAS LÍNEAS 31-32
@@ -18,18 +17,15 @@ app.listen(3000, () => {
 //   res.sendFile(path.join(__dirname, '/views/index.html'))
 // })
 
-app.get('/login',(req,res)=>{
-  res.sendFile(path.join(__dirname,'/views/login.html'))
-});
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/login.html'))
+})
 
 // DESCOMENTAR LÍNEAS 26-28 SI NO FUNCIONA EL .use DE LAS LÍNEAS 31-32
 // app.get('/shoppingCar',(req,res)=>{
 //   res.sendFile(path.join(__dirname,'/views/shoppingCar.html'))
 // });
 
-
+app.use('/product', routeProduct)
 app.use('/', routeIndex)
 app.use('/shoppingCar', routeShoppingCar)
-
-
-
