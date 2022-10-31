777a67567385e5548b110efcf730b8df809de918
BravoArturo <aarturoo97@gmail.com>
2022-10-31 17:10:56 -0300

    se agregó la configuración para trabajar con el metodo post

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ const routerLogin = require('./routes/loginRoute')
 const routeIndex = require('./routes/index')
 const routeShoppingCar = require('./routes/shoppingCar')
 
-const routeProductDetail = require ('./routes/productDetail')
+const routeProductDetail = require('./routes/productDetail')
 
 const routeProduct = require('./routes/product')
 const routeRegister = require('./routes/register')
-
 
 app.set('view engine', 'ejs')
 app.use(express.static(publicPath))
@@ -18,11 +17,10 @@ app.use('/login-mobil', routerLogin)
 app.use('/product', routeProduct)
 app.use('/', routeIndex)
 app.use('/shoppingCar', routeShoppingCar)
-
 app.use('/productDetail', routeProductDetail)
-
 app.use('/register', routeRegister)
-
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 app.listen(3000, () => {
   console.log('Servidor OK en puerto', 'http://localhost:3000')
