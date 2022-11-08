d83fcada7eee07e571560b78201b1b1b7429f944
BravoArturo <aarturoo97@gmail.com>
2022-11-07 23:05:21 -0300

    avance trabajo en grupo

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,28 @@
 const controller = {
+  index: (req, res) => {
+    res.render('products')
+  },
   add: (req, res) => {
     res.render('editAddProduct', { id: null })
   },
   edit: (req, res) => {
+    console.log(req.params.id)
     res.render('editAddProduct', { id: req.params.id })
   },
+  productDetail: (req, res) => {
+    res.render('productDetail')
+  },
+  store: (req, res) => {
+    console.log(req.body)
+    res.redirect('/products')
+  },
+  put: (req, res) => {
+    console.log(req.body)
+    //res.redirect('/products')
+  },
+  // delete: (req, res) => {
+  //   res.redirect('/products')
+  // },
 }
 
 module.exports = controller
--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,12 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/productController')
 
-router.get('/', controller.add)
-router.get('/:id', controller.edit)
+router.get('/', controller.index)
+router.get('/create', controller.add)
+router.post('/', controller.store)
+router.get('/:id', controller.productDetail)
+router.get('/:id/edit', controller.edit)
+router.put('/:id', controller.put)
+// router.delete('/:id', controller.delete)
 
 module.exports = router
