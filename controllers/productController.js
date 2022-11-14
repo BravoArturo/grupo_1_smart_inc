e6e3fa79fdb0acb440806179318194d9464ae3a9
BravoArturo <aarturoo97@gmail.com>
2022-11-14 10:29:42 -0300

    se pasa un json al archivo products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,32 +1,64 @@
 const controller = {
   index: (req, res) => {
-    res.render('products')
+    res.render("products", {
+      products: [
+        {
+          imgSource: "/images/product/SMART-TV-1.jpg",
+          name: "QLED TV",
+          price: 5000,
+        },
+      ],
+    });
   },
   add: (req, res) => {
-    res.render('editAddProduct')
+    res.render("editAddProduct");
   },
   edit: (req, res) => {
-    console.log(req.params.id)
-    res.render('editAddProduct', { id: req.params.id })
+    console.log(req.params.id);
+    res.render("editAddProduct", { id: req.params.id });
   },
   productDetail: (req, res) => {
-    res.render('productDetail')
+    res.render("productDetail");
   },
   store: (req, res) => {
     if (req.file) {
-      console.log(req.file)
+      console.log(req.file);
     }
-    console.log(req.body)
-    res.redirect('/products')
+    console.log(req.body);
+    res.redirect("/products", {
+      products: [
+        {
+          imgSource: "/images/product/SMART-TV-1.jpg",
+          name: "QLED TV",
+          price: 5000,
+        },
+      ],
+    });
   },
   put: (req, res) => {
-    console.log(req.body)
-    res.redirect('/products')
+    console.log(req.body);
+    res.redirect("/products", {
+      products: [
+        {
+          imgSource: "/images/product/SMART-TV-1.jpg",
+          name: "QLED TV",
+          price: 5000,
+        },
+      ],
+    });
   },
   delete: (req, res) => {
-    console.log('req delete', req)
-    res.redirect('/products')
+    console.log("req delete", req);
+    res.redirect("/products", {
+      products: [
+        {
+          imgSource: "/images/product/SMART-TV-1.jpg",
+          name: "QLED TV",
+          price: 5000,
+        },
+      ],
+    });
   },
-}
+};
 
-module.exports = controller
+module.exports = controller;
