0e47cb2e21ef1d474ed6f88c3ff532c1ba1fd578
DFCH9503 <dafechaparro@hotmail.com>
2022-11-20 08:17:33 -0500

    añadiendo avatars de usuarios y procesamiento del register

--- controllers/registerControllers.js
+++ controllers/registerControllers.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const bcryptjs=require('bcryptjs')
 const {validationResult}=require('express-validator')
+const User = require('../models/User');
 
 const registerController = {
 index: (req, res) => {
@@ -9,8 +10,8 @@ index: (req, res) => {
 processRegister: (req,res)=>{
     
     const resultValidation=validationResult(req);
-    const errorsConsole=resultValidation.mapped();
-    console.log(errorsConsole);
+    // const errorsConsole=resultValidation.mapped();
+    // console.log(errorsConsole);
     // console.log(resultValidation);
     // console.log(req.body);
 
@@ -20,6 +21,30 @@ processRegister: (req,res)=>{
             oldData: req.body
         });
     }
+
+    let userInDBbyEmail = User.findByField('email', req.body.email);
+    let userInDBbyuserName = User.findByField('email', req.body.userName);
+
+		if (userInDBbyEmail || userInDBbyuserName) {
+			return res.render('register', {
+				errors: {
+					email: {
+						msg: 'Este email ya está registrado'
+					}
+				},
+				oldData: req.body
+			});
+		}
+
+        let userToCreate = {
+			...req.body,
+			password: bcryptjs.hashSync(req.body.password, 12),
+			avatar: req.file.filename
+		}
+
+        let userCreated = User.create(userToCreate);
+
+        return res.redirect('/login');
 
 }
 }
